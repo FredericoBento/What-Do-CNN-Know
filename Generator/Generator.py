6d7271a7417bb42ffa3b2eb111b539e9a7277133
Frederico Bento <fredbento39@gmail.com>
2024-03-14 12:33:42 +0000

    Dataset Graphs

--- Generator/Generator.py
+++ Generator/Generator.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Rectangle
 from shapely import geometry
-import math
+from scipy.interpolate import interp1d
+
 
 class Generator:
     def __init__(self, image_width=500, image_height=500, seed=None):
@@ -11,16 +12,15 @@ class Generator:
         self.image_height = image_height
         self.seed = seed
 
-        self.circle_ratio = []
-        self.square_ratio = []
-        self.circle_square_ratio = []
-
         self.min_square_side_length = 20
         self.min_circle_radius = 10
-        self.max_circle_radius = image_width / 4
+        self.max_circle_radius = (image_width / 4) + 15
         self.max_square_side_length = image_width / 2
 
-        if seed == None:
+        self.squares_area = []
+        self.circle_area = []
+
+        if seed is None:
             rand_seed = np.random.randint(0, 1000)
             np.random.seed(rand_seed)
             self.seed = rand_seed
@@ -30,7 +30,6 @@ class Generator:
             np.random.seed(seed)
 
     def generate_images(self, draw_random=False, draw_circle=False, draw_square=False, directory="dataset", quantity=1):
-        # np.random.seed(self.seed)
         i = 0
 
         while i < quantity:
@@ -38,7 +37,7 @@ class Generator:
                 draw_circle = np.random.choice([True, False])
                 draw_square = np.random.choice([True, False])
             background_color = self._generate_nonmatching_color()
-            
+
             fig = plt.figure(figsize=(self.image_width/100, self.image_height/100), facecolor=background_color, linewidth=0.0)
             ax = fig.add_subplot(111)
             ax.set_rasterized(True)
@@ -49,9 +48,9 @@ class Generator:
                 square_color = self._generate_nonmatching_color(background_color)
                 side_length = np.random.uniform(self.min_square_side_length, self.max_square_side_length)
                 square_x = np.random.uniform(0, self.image_width - side_length)
-                square_y = np.random.uniform(0, self.image_height - side_length)   
-                square_angle = np.random.uniform(0, 360)                   
-              
+                square_y = np.random.uniform(0, self.image_height - side_length)
+                square_angle = np.random.uniform(0, 360)
+
                 center_x = square_x + side_length / 2
                 center_y = square_y + side_length / 2
                 square = Rectangle((square_x, square_y), side_length, side_length, color=square_color, angle=square_angle, rotation_point=(center_x, center_y))
@@ -63,13 +62,12 @@ class Generator:
                 if self._cornerOutOfBounds(corners[0][0], corners[0][1]) or \
                     self._cornerOutOfBounds(corners[1][0], corners[1][1]) or \
                     self._cornerOutOfBounds(corners[2][0], corners[2][1]) or \
-                    self._cornerOutOfBounds(corners[3][0], corners[3][1]):
+                        self._cornerOutOfBounds(corners[3][0], corners[3][1]):
                     plt.close()
                     continue
 
-               
                 ax.add_patch(square)
-                # change ratio based on size
+                self.squares_area.append(side_length * side_length)
             if draw_circle:
                 if draw_square:
                     filename += "circle_"
@@ -86,38 +84,28 @@ class Generator:
                     if shape_square.intersects(shape_circle):
                         plt.close()
                         continue
-       
 
                 # Check if circle is outside image bounds
                 if circle_x - radius < 0 or circle_x + radius > self.image_width or \
-                    circle_y - radius < 0 or circle_y + radius > self.image_height:
+                        circle_y - radius < 0 or circle_y + radius > self.image_height:
                     plt.close()
                     continue
-            
+
                 circle = Circle((circle_x, circle_y), radius, color=circle_color)
                 circle.set_antialiased(True)
                 ax.add_patch(circle)
-                # change ratio based on size
-                
+                self.circle_area.append((radius**2) * np.pi)
 
             ax.set_xlim(0, self.image_width)
             ax.set_ylim(0, self.image_height)
             ax.set_aspect('equal', adjustable='box')
             ax.axis('off')
-            if draw_square == False and draw_circle == False:
+            if draw_square is False and draw_circle is False:
                 filename = "none_"
             plt.tight_layout()
             plt.savefig(f'{directory + "/" + filename + str(i+1)}.png', bbox_inches='tight', pad_inches=0, dpi=106.5)
             plt.close()
-            if draw_square and draw_circle:
-                self.circle_square_ratio.append((radius * 2) / side_length)
-            else:
-                if draw_square:
-                    self.square_ratio.append(side_length / self.image_width)
-                if draw_circle:
-                    self.circle_ratio.append((radius * 2) / self.image_width)
-            
-            
+
             self.num_images += 1
             i += 1
 
@@ -126,29 +114,83 @@ class Generator:
             color = np.random.rand(3)
             if all(np.linalg.norm(color - excluded) > 0.1 for excluded in excluded_colors):
                 return color
-    
-    
 
     def _cornerOutOfBounds(self, x, y):
         if x < 0:
             return True
         if x > self.image_width:
             return True
-        
+
         if y < 0:
             return True
         if y > self.image_height:
             return True
 
         return False
-    
+
     def getAverageRatio(self):
         print("Circle: size: " + str(len(self.circle_ratio)))
         print("Square: size: " + str(len(self.square_ratio)))
         print("Circle_Square: size: " + str(len(self.circle_square_ratio)))
         print("Number Images" + str(self.num_images))
         return (np.mean(self.circle_ratio).round(2),
-                 np.mean(self.square_ratio).round(2),
-                   np.mean(self.circle_square_ratio).round(2))
+                np.mean(self.square_ratio).round(2),
+                np.mean(self.circle_square_ratio).round(2))
 
-        
+    def getAreaHistogram(self, circle_areas=None, square_areas=None, num_bins=30, folder=None):
+        if circle_areas is None:
+            circle_areas = self.circle_area
+
+        if square_areas is None:
+            square_areas = self.squares_area
+
+        print(str(len(square_areas)))
+        print(str(len(circle_areas)))
+        data = [square_areas, circle_areas]
+        hist = plt.hist(data, num_bins, color=['blue', 'red'], alpha=0.7, label=['Square', 'Circle'])
+        plt.xlabel("Areas")
+        plt.ylabel("Number of Samples")
+        plt.title("Square and Circle Area Comparison")
+        plt.legend()
+        plt.grid(True)
+        fig = plt.gcf()
+        fig.set_size_inches(10, 5)
+        filename = 'Dataset_Histogram_seed_' + str(self.seed) + '.png'
+        if folder is not None:
+            filename = folder + "/" + filename
+        fig.savefig(filename + str(self.seed) + '.png', dpi=100)
+        plt.close()
+
+    def getAreaLineGraph(self, circle_areas=None, square_areas=None, folder=None):
+        if circle_areas is None:
+            circle_areas = self.circle_area
+
+        if square_areas is None:
+            square_areas = self.squares_area
+
+        square_areas = np.sort(square_areas)
+        circle_areas = np.sort(circle_areas)
+
+        x1 = np.linspace(0, 1, len(square_areas))
+        x2 = np.linspace(0, 1, len(circle_areas))
+
+        # Interpolate data2 to match the length of data1
+        f = interp1d(x2, circle_areas)
+        data2_interp = f(x1)
+
+        # Plot the line graph
+        plt.plot(x1, square_areas, color='blue', label='Square Areas (' + str(len(square_areas)) + ')')
+        plt.plot(x1, data2_interp, color='red', label='Circle Areas' + str(len(circle_areas)) + ')')
+
+        plt.xlabel('Number of Samples')
+        plt.ylabel('Areas')
+        plt.title('Comparison of Areas')
+        plt.legend()
+        plt.grid(True)
+        fig = plt.gcf()
+        fig.set_size_inches(10, 5)
+        filename = 'Dataset_Line_seed_' + str(self.seed) + '.png'
+        if folder is not None:
+            filename = folder + "/" + filename
+        fig.savefig(filename + str(self.seed) + '.png', dpi=100)
+        plt.close()
--- Generator/main.py
+++ Generator/main.py
@@ -2,8 +2,11 @@ from Generator import Generator
 import os
 from time import perf_counter as pc
 
-TRAIN_DIRECTORY = "dataset3/train/"
-TEST_DIRECTORY = "dataset3/test/"
+DATASET_DIRECTORY_NAME = "dataset"
+DATASET_GRAPH_DIRECTORY = DATASET_DIRECTORY_NAME + "/graphs"
+
+TRAIN_DIRECTORY = DATASET_DIRECTORY_NAME + "/train/"
+TEST_DIRECTORY = DATASET_DIRECTORY_NAME + "/test/"
 
 CIRCLE_TRAIN_DIRECTORY = TRAIN_DIRECTORY + "circle"
 NONE_TRAIN_DIRECTORY = TRAIN_DIRECTORY + "none"
@@ -18,15 +21,19 @@ SQUARE_TEST_DIRECTORY = TEST_DIRECTORY + "square"
 SQUARE_CIRCLE_TEST_DIRECTORY = TEST_DIRECTORY + "square_circle"
 
 folders = [CIRCLE_TRAIN_DIRECTORY, NONE_TRAIN_DIRECTORY,
-        SQUARE_TRAIN_DIRECTORY, SQUARE_CIRCLE_TRAIN_DIRECTORY,
-        CIRCLE_TEST_DIRECTORY, NONE_TEST_DIRECTORY,
-        SQUARE_TEST_DIRECTORY, SQUARE_CIRCLE_TEST_DIRECTORY]
+           SQUARE_TRAIN_DIRECTORY, SQUARE_CIRCLE_TRAIN_DIRECTORY,
+           CIRCLE_TEST_DIRECTORY, NONE_TEST_DIRECTORY,
+           SQUARE_TEST_DIRECTORY, SQUARE_CIRCLE_TEST_DIRECTORY,
+           DATASET_GRAPH_DIRECTORY]
+
 
 def del_dir(rootdir):
-    if(os.path.isdir(rootdir)):
+    if os.path.isdir(rootdir):
         for (dirpath, dirnames, filenames) in os.walk(rootdir):
-            for filename in filenames: os.remove(rootdir+'/'+filename)
-            for dirname in dirnames: del_dir(rootdir+'/'+dirname)
+            for filename in filenames:
+                os.remove(rootdir+'/'+filename)
+            for dirname in dirnames:
+                del_dir(rootdir+'/'+dirname)
         os.rmdir(rootdir)
 
 
@@ -43,8 +50,9 @@ test_quantity = 100
 train_quantity = 300 / 4
 
 print("Starting to generate images")
-generator = Generator(seed=891)
+generator = Generator()
 start = pc()
+
 generator.generate_images(draw_random=True, directory=TEST_DIRECTORY, quantity=test_quantity)
 
 generator.generate_images(False, False, True, SQUARE_TRAIN_DIRECTORY, train_quantity)
@@ -53,29 +61,25 @@ generator.generate_images(False, False, False, NONE_TRAIN_DIRECTORY, train_quant
 generator.generate_images(False, True, True, SQUARE_CIRCLE_TRAIN_DIRECTORY, train_quantity)
 
 
-
-#move image from test to subdirectory
+# move image from test to subdirectory
 for filename in os.listdir(TEST_DIRECTORY):
-    #skip dir
     if os.path.isdir(TEST_DIRECTORY + filename):
         continue
     if filename.__contains__("square_circle_"):
-       os.rename(TEST_DIRECTORY + filename, SQUARE_CIRCLE_TEST_DIRECTORY + "/" + filename)
+        os.rename(TEST_DIRECTORY + filename, SQUARE_CIRCLE_TEST_DIRECTORY + "/" + filename)
     elif filename.__contains__("circle_"):
         os.rename(TEST_DIRECTORY + filename, CIRCLE_TEST_DIRECTORY + "/" + filename)
     elif filename.__contains__("square_"):
         os.rename(TEST_DIRECTORY + filename, SQUARE_TEST_DIRECTORY + "/" + filename)
     else:
         os.rename(TEST_DIRECTORY + filename, NONE_TEST_DIRECTORY + "/" + filename)
-    
+
 # Save seed to file
-with open("dataset1/seed.txt", "w") as f:
+with open(DATASET_DIRECTORY_NAME + "/seed.txt", "w") as f:
     f.write(str(generator.seed))
 
 end = pc()
-print("Finished generating images in " + str(end - start ) + " seconds")
+print("Finished generating images in " + str(end - start) + " seconds")
 
-(c_ratio, s_ratio, cs_ratio) = generator.getAverageRatio() 
-print("Circle Ratio: " + str(c_ratio))  
-print("Square Ratio: " + str(s_ratio))
-print("Circle Square Ratio: " + str(cs_ratio))
+generator.getAreaHistogram(folder=DATASET_GRAPH_DIRECTORY)
+generator.getAreaLineGraph(folder=DATASET_GRAPH_DIRECTORY)
