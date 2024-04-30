a61698691adb85f4c00dcba0ba6d08147c6fe91e
Frederico Bento <fredbento39@gmail.com>
2024-05-01 00:26:18 +0100

    Dataset 7_1
    Fixing intersection
    anti Intersect block not working (white background??)

--- Dataset_Scripts/dataset_7_1.py
+++ Dataset_Scripts/dataset_7_1.py
@@ -38,26 +38,27 @@ start = pc()
 sar_sq_writer = csv.writer(open(os.path.join(data_folder, 'sar_sq.csv'), 'w'))
 sar_ci_writer = csv.writer(open(os.path.join(data_folder, 'sar_ci.csv'), 'w'))
 sar_ci_writer.writerow(['Filename', 'X', 'Y', 'Radius', 'Area', 'Visible Area', 'Distance From Center',
-                        'Distance From Center Further', 'Color', 'Bg_color', 'Position', 'Cut', 'Variant'])
+                        'Distance From Center Further', 'Color', 'Bg_color', 'Position', 'Intersected', 'Cut', 'Variant'])
 
 sar_sq_writer.writerow(['Filename', 'X', 'Y', 'Length', 'Angle', 'Area', 'Visible Area', 'Distance From Center'
                         , 'Distance From Center Further',
-                        'Corners', 'Color', 'Bg_color', 'Position', 'Cut', 'Variant'])
+                        'Corners', 'Color', 'Bg_color', 'Position', 'Intersected', 'Cut', 'Variant'])
 
 car_sq_writer = csv.writer(open(os.path.join(data_folder, 'car_sq.csv'), 'w'))
 car_ci_writer = csv.writer(open(os.path.join(data_folder, 'car_ci.csv'), 'w'))
 car_ci_writer.writerow(['Filename', 'X', 'Y', 'Radius', 'Area', 'Visible Area', 'Distance From Center',
-                        'Distance From Center Further', 'Color', 'Bg_color', 'Position', 'Cut', 'Variant'])
+                        'Distance From Center Further', 'Color', 'Bg_color', 'Position', 'Intersected', 'Cut', 'Variant'])
 
 car_sq_writer.writerow(['Filename', 'X', 'Y', 'Length', 'Angle', 'Area', 'Visible Area', 'Distance From Center'
                         , 'Distance From Center Further',
-                        'Corners', 'Color', 'Bg_color', 'Position', 'Cut', 'Variant'])
+                        'Corners', 'Color', 'Bg_color', 'Position', 'Intersected', 'Cut', 'Variant'])
 
 max_square_area = 150 ** 2
 # car (circle at right)
 fig = plt.figure(figsize=(img_width/100, img_height/100))
 fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
 counter = 1
+
 for j in range(2):
     if j == 0:
         variant = 'Train'
@@ -66,18 +67,30 @@ for j in range(2):
         variant = 'Test'
         size = test_size
 
-    distribution = np.random.uniform(min_square_length, 150, size)
+    distribution = np.random.uniform(min_square_length, 150, int(size / 2))
+    dist_idx = 0
     do_2_ci = False
+    intersection_counter = 0
+    intersection_counter_reached = False
+    intersection_under = 0 # Counts the number of intersections where the circle is under the other shape
     for i in range(size):
         if i >= size / 2:
             do_2_ci = True
+
+        if (intersection_counter >= (size / 2)) and (intersection_counter_reached is False):
+            print(intersection_counter_reached)
+            distribution = np.random.uniform(min_square_length, 150, int(size / 2))
+            intersection_counter_reached = True
+            dist_idx = 0
 
         b = f"Generating car {i+1}/{size} "
         print(b, end='\r', flush=True)
         bg_color = du.generate_nonmatching_color()
         ax = fig.add_subplot(111, aspect='auto')
         excluded_colors = [bg_color]
-        dist_value = distribution[i]
+        if dist_idx >= len(distribution):
+            dist_idx -= 1
+        dist_value = distribution[dist_idx]
         radius = dist_value / 2
         sq_intersected = False
         ci_intersected = False
@@ -98,9 +111,9 @@ for j in range(2):
                 if du.square_out_of_bounds(corners, img_width, img_height) is True:
                     continue
 
-                if du.square_is_at_right(corners, img_width, img_height):
-                    continue
-
+                # if du.square_is_at_right(corners, img_width, img_height):
+                #     continue
+                #
                 all_ok = True
 
             sq_color = du.generate_nonmatching_color(excluded_colors)
@@ -121,8 +134,8 @@ for j in range(2):
                 if du.circle_out_of_bounds(ci_left_x, ci_left_y, radius, img_width, img_height):
                     continue
 
-                if du.circle_is_at_right(ci_left_x, radius, img_width):
-                    continue
+                # if du.circle_is_at_right(ci_left_x, radius, img_width):
+                #     continue
 
                 all_ok = True
 
@@ -135,16 +148,39 @@ for j in range(2):
             cal = patches.Circle((ci_left_x, ci_left_y), radius, color=ci_left_color)
 
         all_ok = False
+        anti_intersect_block = False
+        tries = 0
+        max_tries = 100
         while all_ok is False:
             # add the circle at right
-            ci_x = np.random.uniform(0 + radius, img_width - radius)
-            ci_y = np.random.uniform(0 + radius, img_height - radius)
+            if tries >= max_tries:
+                anti_intersect_block = True
+                break
+
+            if intersection_counter_reached is False:
+                # force intersection
+                intersect = False
+                if do_2_ci is False:
+                    while intersect is False:
+                        ci_x = np.random.uniform(sq_center_x - radius, sq_center_x + radius)
+                        ci_y = np.random.uniform(sq_center_y - radius, sq_center_y + radius)
+                        intersect = du.circle_intersect_square(ci_x, ci_y, radius, corners)
+                else:
+                    while intersect is False:
+                        ci_x = np.random.uniform(ci_left_x - radius, ci_left_x + radius)
+                        ci_y = np.random.uniform(ci_left_y - radius, ci_left_y + radius)
+                        intersect = du.circle_intersect_circle(ci_x, ci_y, radius, ci_left_x, ci_left_y, radius)
+            else:
+                ci_x = np.random.uniform(0 + radius, img_width - radius)
+                ci_y = np.random.uniform(0 + radius, img_height - radius)
+                print(ci_x, ci_y)
 
             if du.circle_out_of_bounds(ci_x, ci_y, radius, img_width, img_height):
                 continue
 
-            if du.circle_is_at_right(ci_x, radius, img_width) is False:
-                continue
+            # if du.circle_is_at_right(ci_x, radius, img_width) is False:
+            #     print("not at right")
+            #     continue
 
             # check if square is more to the right
             sqar = False
@@ -161,75 +197,90 @@ for j in range(2):
             if do_2_ci is False:
                 if du.circle_intersect_square(ci_x, ci_y, radius, corners) is True:
                     sq_intersected = True
+                    intersection_counter += 1
             else:
                 if du.circle_intersect_circle(ci_x, ci_y, radius, ci_left_x, ci_left_y, radius) is True:
                     ci_intersected = True
+                    intersection_counter += 1
 
             all_ok = True
+            if sq_intersected is True or ci_intersected is True:
+                if intersection_counter_reached is True:
+                    print("Intersected")
+                    print(radius)
+                    all_ok = False
+                    intersection_counter -= 1
+                    tries += 1
 
-        ci_color = du.generate_nonmatching_color(excluded_colors)
-        ci_dfc = du.calculate_dfc_circle(ci_x, ci_y, radius, img_width, img_height)
-        ci_dfc_f = du.calculate_dfc_further_circle(ci_x, ci_y, radius, img_width, img_height)
-        ci_area = round(np.pi * radius ** 2, 2)
-        ci_visible_area = ci_area
-        car = patches.Circle((ci_x, ci_y), radius, color=ci_color)
+        if anti_intersect_block is False:
+            ci_color = du.generate_nonmatching_color(excluded_colors)
+            ci_dfc = du.calculate_dfc_circle(ci_x, ci_y, radius, img_width, img_height)
+            ci_dfc_f = du.calculate_dfc_further_circle(ci_x, ci_y, radius, img_width, img_height)
+            ci_area = round(np.pi * radius ** 2, 2)
+            ci_visible_area = ci_area
+            car = patches.Circle((ci_x, ci_y), radius, color=ci_color)
 
-        # handle intersected visible areas
-        if sq_intersected is True or ci_intersected is True:
-            # decide if circle is under or above
-            put_circle_under = np.random.choice([True, False])
-            if put_circle_under is True:
-                print(counter)
-                if sq_intersected is True:
-                    intersected_area = du.calculate_intersect_area_sq_ci(corners, ci_x, ci_y, radius)
-                    square.set_color(sq_color)
-                    square.zorder = 2
-                    ax.add_patch(square)
+            # handle intersected visible areas
+            if sq_intersected is True or ci_intersected is True:
+                # decide if circle is under or above
+                put_circle_under = np.random.choice([True, False])
+                if intersection_counter < size / 4:
+                    put_circle_under = True
                 else:
-                    intersected_area = du.calculate_intersect_area_ci_ci(ci_x, ci_y, radius, ci_left_x, ci_left_y, radius)
-                    cal.zorder = 2
-                    ax.add_patch(cal)
+                    put_circle_under = False
 
-                ci_visible_area = ci_visible_area - intersected_area
+                if put_circle_under is True:
+                    if sq_intersected is True:
+                        intersected_area = du.calculate_intersect_area_sq_ci(corners, ci_x, ci_y, radius)
+                        square.set_color(sq_color)
+                        square.zorder = 2
+                        ax.add_patch(square)
+                    else:
+                        intersected_area = du.calculate_intersect_area_ci_ci(ci_x, ci_y, radius, ci_left_x, ci_left_y, radius)
+                        cal.zorder = 2
+                        ax.add_patch(cal)
+
+                    ci_visible_area = ci_visible_area - intersected_area
+                else:
+                    if sq_intersected is True:
+                        intersected_area = du.calculate_intersect_area_sq_ci(corners, ci_x, ci_y, radius)
+                        sq_visible_area = sq_visible_area - intersected_area
+                        ax.add_patch(square)
+                    else:
+                        intersected_area = du.calculate_intersect_area_ci_ci(ci_x, ci_y, radius, ci_left_x, ci_left_y, radius)
+                        ci_left_visible_area = ci_left_visible_area - intersected_area
+                        ax.add_patch(cal)
+                    car.zorder = 2
+
+            ax.add_patch(car)
+
+            # write circle at right
+            car_ci_writer.writerow([f'car_{counter}.png', ci_x, ci_y, radius, ci_area, ci_area, ci_dfc, ci_dfc_f, ci_color, bg_color, "Right", "True", variant])
+
+            if do_2_ci is False:
+                # write and add square at left
+                car_sq_writer.writerow([f'car_{counter}.png', sq_x, sq_y, length, angle, sq_area, sq_visible_area, sq_dfc, sq_dfc_f, corners, sq_color, bg_color, "Left", "False", variant])
+                if sq_intersected is False:
+                    ax.add_patch(square)
             else:
-                if sq_intersected is True:
-                    intersected_area = du.calculate_intersect_area_sq_ci(corners, ci_x, ci_y, radius)
-                    sq_visible_area = sq_visible_area - intersected_area
-                    ax.add_patch(square)
-                else:
-                    intersected_area = du.calculate_intersect_area_ci_ci(ci_x, ci_y, radius, ci_left_x, ci_left_y, radius)
-                    ci_left_visible_area = ci_left_visible_area - intersected_area
+                # write and add circle at left
+                car_ci_writer.writerow([f'car_{counter}.png', ci_left_x, ci_left_y, radius, ci_left_area, ci_left_visible_area, ci_left_dfc, ci_left_dfc_f, ci_left_color, bg_color, "Left", "True", variant])
+                if ci_intersected is False:
                     ax.add_patch(cal)
-                car.zorder = 2
 
-        ax.add_patch(car)
-
-        # write circle at right
-        car_ci_writer.writerow([f'car_{counter}.png', ci_x, ci_y, radius, ci_area, ci_area, ci_dfc, ci_dfc_f, ci_color, bg_color, "Right", "True", variant])
-
-        if do_2_ci is False:
-            # write and add square at left
-            car_sq_writer.writerow([f'car_{counter}.png', sq_x, sq_y, length, angle, sq_area, sq_visible_area, sq_dfc, sq_dfc_f, corners, sq_color, bg_color, "Left", "False", variant])
-            if sq_intersected is False:
-                ax.add_patch(square)
-        else:
-            # write and add circle at left
-            car_ci_writer.writerow([f'car_{counter}.png', ci_left_x, ci_left_y, radius, ci_left_area, ci_left_visible_area, ci_left_dfc, ci_left_dfc_f, ci_left_color, bg_color, "Left", "True", variant])
-            if ci_intersected is False:
-                ax.add_patch(cal)
-
-        fig.set_facecolor(bg_color)
-        ax.set_xlim(0, img_width)
-        ax.set_ylim(0, img_height)
-        ax.axis('off')
-        if j == 0:
-            folder = car_folder_train
-        else:
-            folder = car_folder_test
-        path = os.path.join(folder, f'car_{counter}.png')
-        plt.savefig(path, bbox_inches=None, pad_inches=0, dpi=100)
-        plt.clf()
-        counter += 1
+            fig.set_facecolor(bg_color)
+            ax.set_xlim(0, img_width)
+            ax.set_ylim(0, img_height)
+            ax.axis('off')
+            if j == 0:
+                folder = car_folder_train
+            else:
+                folder = car_folder_test
+            path = os.path.join(folder, f'car_{counter}.png')
+            plt.savefig(path, bbox_inches=None, pad_inches=0, dpi=100)
+            plt.clf()
+            counter += 1
+        dist_idx += 1
 
 # sar (square at right)
 counter = 1
